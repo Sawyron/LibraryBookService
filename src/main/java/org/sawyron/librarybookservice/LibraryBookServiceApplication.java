b3d840498caa537892f9b83c1cc4ca8982e57b63
Sawyron <s4pakvv@gmail.com>
2024-11-04 18:28:47 +0700

    initial commit

---
+++ src/main/java/org/sawyron/librarybookservice/LibraryBookServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sawyron.librarybookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBookServiceApplication.class, args);
+	}
+
+}
